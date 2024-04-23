6f80dae07f30e958852688f0574df791eb2d380b
Muhamad Sumair Shaukat <sumairch224@gmail.com>
2024-04-23 23:48:21 +0500

    add Coffee entity and add options in globabl pipes

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,1 +1,17 @@
-export class Coffee {}
+import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+
+@Entity('coffees')
+export class Coffee {
+    @PrimaryGeneratedColumn()
+    id: number ;
+
+    @Column()
+    name: string;
+
+    @Column()
+    brand: string;
+
+    @Column('json', {nullable: true})
+    flavour: string[];
+}
